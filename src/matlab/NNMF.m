c691b5a7384dc2a489c9c723d4fab120298776ea
Michael Buch <michaelbuch12@gmail.com>
2016-02-18 01:14:47 +0000

    Modified visualization of Activations

--- src/matlab/NNMF.m
+++ src/matlab/NNMF.m
@@ -32,15 +32,17 @@ classdef NNMF
             W = abs(SynthObj.SourceSpectrogram.S);
             T = SynthObj.TargetSpectrogram.T;
             H = NNMFObj.Activations;
-            [~,I]=max(W);
-            [~,Ix] = sort(I,'ascend');
-            imagesc(T,1:size(H,1),max(-20,20*log10(H(Ix,:)./max(H(:))))); %TODO: Reconsider dB level
+%             [~,I]=max(W);
+%             [~,Ix] = sort(I,'ascend');
+%             imagesc(T,1:size(H,1),max(-20,20*log10(H(Ix,:)./max(H(:))))); %TODO: Reconsider dB level
+            imagesc(max(-20,20*log10(H./max(max((H))))));
+%             imagesc(H);
             cmap = colormap('gray');
             cmap(1,:) = 0*ones(1,3);
             colormap(flipud(cmap))
             colorbar
             axis xy; grid on;
-            set(gca,'XTick',[0:0.5:T(end)],'XTickLabel','');
+%             set(gca,'XTick',[0:0.5:T(end)],'XTickLabel','');
             set(gca, 'Layer', 'top');
             ylabel('Template');
             grid on;
--- src/matlab/drawClickCallBack.m
+++ src/matlab/drawClickCallBack.m
@@ -1,7 +1,6 @@
 %Used for the plot drawing function
 function drawClickCallBack(src,callbackdata, action)
 handles = guidata(src);
-src.Pointer = 'circle';
 ah = handles.axes1;
 xAxisProps = get(ah, 'XAxis');
 xLimits = get(xAxisProps, 'Limits')
@@ -12,27 +11,36 @@ xUnit = xLimits/size(acts, 2);
 sliderHandle = handles.slider3;
 
 if(strcmp(action, 'Draw'))
+    src.Pointer = 'cross';
     fillValue = get(sliderHandle, 'Value')
 elseif(strcmp(action, 'Erase'))
+	src.Pointer = 'circle';
     fillValue = 0;
 end
-% currentplot = get(ah, 'Children');
-% currentxdata = get(currentplot, 'XData');
-% currentydata = get(currentplot, 'YData');
+
+cp = ah.CurrentPoint;
+cx = cp(1,1)/xUnit(2)
+cy = cp(1,2)
+acts(ceil(cy), ceil(cx)) = fillValue;
+handles.SynthesisObject.NNMFSynthesis.Activations = acts;
+size(acts, 1)
+guidata(src, handles);
 
 src.WindowButtonMotionFcn = @dragCallBack;
 src.WindowButtonUpFcn = @releaseCallBack;
 
     function dragCallBack(src, callbackdata)
         cp = ah.CurrentPoint;
-        cx = cp(1,1)/xUnit(2)
-        cy = cp(1,2)
+        cx = cp(1,1)/xUnit(2);
+        cy = cp(1,2);
+%         fprintf('X: %u Y: %u', ceil(cy), ceil(cx))
         acts(ceil(cy), ceil(cx)) = fillValue;
         handles.SynthesisObject.NNMFSynthesis.Activations = acts;
         guidata(src, handles);
     end
 
     function releaseCallBack(src, callbackdata)
+        SynthesisCtr('switchPlot', handles);
         src.Pointer = 'arrow';
         src.WindowButtonMotionFcn = '';
         src.WindowButtonUpFcn = '';
