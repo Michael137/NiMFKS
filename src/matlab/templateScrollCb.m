6e4f58e52be820593cbe329b93ae25d37a38ee3a
Michael Buch <michaelbuch12@gmail.com>
2016-02-20 11:31:26 +0000

    Fixed bugs in Template Manipulation Feature

--- src/matlab/templateScrollCb.m
+++ src/matlab/templateScrollCb.m
@@ -1,15 +1,22 @@
 function templateScrollCb(src, callbackdata, handles)
 
 selectedTemplate = findobj(gca, 'Color', 'b');
+if(callbackdata.VerticalScrollCount < 0 && size(selectedTemplate, 1) ~= 0) %Up scroll
+    newTemplateIndex = length(handles) - selectedTemplate.XData(1) + 2;
+    if(newTemplateIndex > length(handles))
+        newTemplateIndex = 2;
+    end
+    neighbouringTemplate = newTemplateIndex + 1;
+elseif(callbackdata.VerticalScrollCount > 0 && size(selectedTemplate, 1) ~= 0) %Down scroll
+    newTemplateIndex = length(handles) - selectedTemplate.XData(1);
+    if(newTemplateIndex < 1)
+        newTemplateIndex = length(handles);
+    end
+end
 
 if(size(selectedTemplate, 1) == 0)
     set(handles(1), 'Color', 'b');
 else
-    if(selectedTemplate.XData(1) == 1)
-        set(selectedTemplate, 'Color', get(handles(length(handles)-1), 'Color'));
-        set(handles(1), 'Color', 'b');
-    else
-        set(selectedTemplate, 'Color', get(handles(length(handles) - selectedTemplate.XData(1) + 2), 'Color'));
-        set(handles(length(handles) - selectedTemplate.XData(1) + 2), 'Color', 'b');
-    end
+    set(selectedTemplate, 'Color', get(handles(newTemplateIndex), 'Color'));
+    set(handles(newTemplateIndex), 'Color', 'b');
 end
