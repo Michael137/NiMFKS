ec762ca67b9e501396fa88cc8b73fccb1fe017e5
Michael137 <michaelbuch12@gmail.com>
2017-02-09 00:27:47 +0000

    Improved kernel rotation

--- src/matlab/rot_kernel.m
+++ src/matlab/rot_kernel.m
@@ -15,8 +15,8 @@ else
     for r = 1:num_of_rots
         if mod( r, 2 ) == 0
             kernel = rot_outer_layer( kernel );
-%         else
-%             kernel = rot_inner_layers( kernel );
+        else
+            kernel = rot_inner_layer( kernel );
         end
     end
 end
@@ -31,7 +31,7 @@ function [Y] = rot_outer_layer( A )
     right = A( 2:dim, dim )';
     bottom = fliplr( A( dim, 1:dim - 1 ) );
     left = fliplr( A( 2:dim - 1, 1 )' );
-    shifted_layers = wshift('1D',[top,right,bottom,left],1);
+    shifted_layers = wshift('1D',[top,right,bottom,left],-1);
     
     top = shifted_layers( 1:length(top));
     last_ind = length( top );
@@ -52,9 +52,29 @@ function [Y] = rot_outer_layer( A )
     Y = A;
 end
 
-function [Y] = rot_inner_layers( kernel )
+function [Y] = rot_inner_layer( kernel )
 %     dim = size( kernel, 1 );
-%     for 1:dim
-%         rotate_outer_layer
+%     if  dim < 2
+%         Y = rot_outer_layer( kernel )
+%         return;
+%     else
+%         Y = rot_inner_layer( kernel( 2:dim - 1, 2:dim - 1 ) )
 %     end
+    dim = size( kernel, 1 );
+    
+    if mod( dim / 2, 2 ) == 0
+        max_rots = dim / 2 - 1;
+    else
+        max_rots = dim / 2 - 0.5;
+    end
+    
+    low = 2;
+    high = dim - 1;
+    for i = 1:max_rots
+        kernel( low:high, low:high ) = rot_outer_layer( kernel( low:high, low:high ) );
+        low = low + 1;
+        high = high - 1;
+    end
+    
+    Y = kernel;
 end
